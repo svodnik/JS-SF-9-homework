1d60251681898678970e7a63ceca4c67b4fb3bc5
Haya <hayaodeh2@gmail.com>
2017-11-21 21:37:40 -0800

    submitting homework 1

--- haya-hayaodeh/Homework-1/99-bottles-of-beer/app.js
+++ haya-hayaodeh/Homework-1/99-bottles-of-beer/app.js
@@ -10,3 +10,11 @@
   conditional statements at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else). 
 */
 
+for (let i = 99; i>0 ; i--) {
+  
+ console.log(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+ console.log("Take one down and pass it around, "+ (i-1) + " bottles of beer on the wall.")
+console.log (" ")
+  
+}
+ console.log ("No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.")
--- haya-hayaodeh/Homework-1/fizzbuzz/app.js
+++ haya-hayaodeh/Homework-1/fizzbuzz/app.js
@@ -25,3 +25,19 @@
 // ##### Step 4:
 // Add an additional `else if` clause that logs the string `"fizzbuzz"` if the value being iterated over is divisible by both `3` and `5`.
 
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0) {
+    console.log('fizz');
+    } 
+  if (i % 5 === 0) {
+    console.log('buzz');
+  }  
+    
+  if (i % 5=== 0, i%3===0) {
+    console.log('frizzbuzz');
+  }    
+    
+    else {
+    console.log(i);
+  }
+}
