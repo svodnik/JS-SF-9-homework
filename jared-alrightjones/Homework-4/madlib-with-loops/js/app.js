165f938b3192fcb6926f0a3488e8c38c4b5d7a07
Jared <alrightjones@github.com>
2017-12-17 21:01:07 -0800

    another pass at hw4 madlibs, still not working though.

--- jared-alrightjones/Homework-4/madlib-with-loops/js/app.js
+++ jared-alrightjones/Homework-4/madlib-with-loops/js/app.js
@@ -1,7 +1,4 @@
-
-const createBtn = document.querySelector('#create');
-const saveBtn = document.querySelector('#save');
-const printBtn = document.querySelector('#print');
+// provided code
 
 var startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 var startupY = ['Slack', 'GitHub', 'Tesla', 'Hyperloop', 'Harvest'];
@@ -9,16 +6,19 @@ var startupY = ['Slack', 'GitHub', 'Tesla', 'Hyperloop', 'Harvest'];
 var random1 = Math.floor((Math.random() * startupX.length));
 var random2 = Math.floor((Math.random() * startupY.length));
 
-createBtn.addEventListener('click', function() {
-  something.somethingElse = 'dontForgetFunctionDefinition';
-};
+// create something
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+var greatIdea;
+const createBtn = document.querySelector('#create'); 
+const outputField = document.querySelector('#xForY');
+createBtn.addEventListener('click' outputText)
+
+function outputText() {
+	greatIdea = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+	startup.innerHTML = startupIdea;
+}
+
+// save something
 
 
-// 1. click the createBtn
-// 2. creatBtn runs function to
-// 	a. random1 to startupX selection
-// 	b. random2 to startupY selection
-// 3. console.log satement to add text and combine random1 + random2
-// 4. print the console.log statement the XforY h1
+// print something
